6983c971a4aae2b88d93ea8db4ce29dd124a739a
prateek <stupidcoder22@gmail.com>
2022-03-05 15:48:18 +0530

    half done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentitem, setcurrentitem] = useState("");
+  const [userlist, setuserlist] = useState([]);
+  function onchangehandler(e) {
+    setcurrentitem(e.target.value);
+  }
+  function updatelist() {
+    setuserlist([...userlist, { item: currentitem, key: Date.now() }]);
+    setcurrentitem("");
+    console.log(userlist);
+  }
+  console.log("render");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="wrapper">
+          <div className="inputwrapper">
+            <input type="text" value={currentitem} onChange={onchangehandler} />
+            <button onClick={updatelist}>+</button>
+          </div>
+        </div>
       </header>
     </div>
   );
